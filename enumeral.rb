63ffcb3b180b5176cbaa998527ea150ca3b23cc1
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-05 17:05:51 -0500

    implement my_each, my_each_with_index

---
+++ enumeral.rb
@@ -0,0 +1,25 @@
+module Enumerable
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    arr = []
+    my_each { |value| arr << value if yield(value) }
+    arr
+  end
+end
+
+[1, 2, 3, 4, 5, 6].my_select { |n| n.even? }
