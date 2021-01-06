2120783afecedf5b7a4fe074ed1de04764628223
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-06 13:04:49 -0500

    implement my_map, my_inject, multiply_els

--- enumeral.rb
+++ enumeral.rb
@@ -14,43 +14,75 @@ module Enumerable
       i += 1
     end
   end
-def my_select
-  arr = []
-  my_each { |value| arr << value if yield value}
- print arr
-end
-def my_all?
-  array = []
-  my_each { |value| array << value unless yield value}
-  if array.empty?
-    return true
-  else
-    return false
+
+  def my_select
+    array = []
+    my_each { |value| array << value if yield value }
+    print arr
   end
-end
-def my_any?
-  array = []
-  my_each { |value| array << value if yield value}
-  if array.empty?
-    return false
-  else
-    return true
+
+  def my_all?
+    array = []
+    my_each { |value| array << value unless yield value }
+    if array.empty?
+      return true
+    else
+      return false
+    end
   end
-end
-def my_none?
-  array = []
-  my_each { |value| array << value if yield value}
-  if array.empty?
-    return true
-  else
-    return false
+
+  def my_any?
+    array = []
+    my_each { |value| array << value if yield value }
+    if array.empty?
+      return false
+    else
+      return true
+    end
   end
-end
-def my_count
-  count = 0
-  my_each { |value| count +=1 if yield value}
-  count
-end
+
+  def my_none?
+    array = []
+    my_each { |value| array << value if yield value }
+    if array.empty?
+      return true
+    else
+      return false
+    end
+  end
+
+  def my_count
+    count = 0
+    my_each { |value| count += 1 if yield value }
+    count
+  end
+
+  def my_map(proc = nil)
+    array = []
+    if proc.nil?
+      my_each do |value|
+        value = yield value
+        array << value
+      end
+    else
+      my_each do |value|
+        value = proc.call(value)
+        array << value
+      end
+    end
+    array
+  end
+
+  def my_inject(result = 0)
+    my_each { |value| result = yield(result, value) }
+    result
+  end
+
+  def multiply_els(result = 1)
+    my_each { |value| result = yield(result, value) }
+    result
+  end
 end
 
-puts [ 2, 4, 7, 10, 6, 12].my_count { |n| n % 2 == 0 } 
+my_proc = Proc.new { |n| n.upcase }
+p ["a", "b", "c"].my_map(my_proc)
