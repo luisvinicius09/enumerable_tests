708c25717199ed28e362faa0c2f1a0bc8b2fb6cf
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-07 16:00:14 -0500

    fix methods and add rspec to test

--- enumerables.rb
+++ enumerables.rb
@@ -1,91 +1,121 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+
 module Enumerable
   def my_each
-    if block_given?
-      i = 0
-      while i < length
-        yield(to_a[i])
-        i += 1
-      end
-    else
-      return to_enum
+    return to_enum unless block_given?
+
+    i = 0
+    while i < size
+      yield(to_a[i])
+      i += 1
     end
+    self
   end
 
   def my_each_with_index
+    return to_enum unless block_given?
+
     i = 0
-    while i < length
-      yield(self[i], i)
+    while i < size
+      yield(to_a[i], i)
       i += 1
     end
+    self
   end
 
   def my_select
+    return to_enum unless block_given?
+
     array = []
     my_each { |value| array << value if yield value }
-    print arr
+    array
   end
 
-  def my_all?
+  def my_all?(arg = nil)
     array = []
-    my_each { |value| array << value unless yield value }
-    if array.empty?
-      true
+    if block_given?
+      my_each { |value| array << value unless yield value }
+    elsif arg.is_a? Regexp
+      my_each { |value| array << value unless value.to_s =~ arg }
+    elsif arg
+      my_each { |value| array << value unless value == arg }
     else
-      false
+      my_each { |value| array << value unless !value || value.nil? }
     end
+    array.empty? ? true : false
   end
 
-  def my_any?
-    array = []
-    my_each { |value| array << value if yield value }
-    if array.empty?
-      false
+  def my_any?(arg = nil)
+    if block_given?
+      my_each { |value| return true if yield value }
+    elsif arg.is_a? Regexp
+      my_each { |value| return true if value.to_s =~ arg }
+    elsif arg
+      my_each { |value| return true if value == arg }
     else
-      true
+      my_each { |value| return true if value }
     end
+    false
   end
 
-  def my_none?
-    array = []
-    my_each { |value| array << value if yield value }
-    if array.empty?
-      true
+  def my_none?(arg = nil)
+    if block_given?
+      my_each { |value| return false if yield value }
+    elsif arg.is_a? Regexp
+      my_each { |value| return false if value.to_s =~ arg }
+    elsif arg
+      my_each { |value| return false if value == arg }
     else
-      false
+      my_each { |value| return false if value }
     end
+    true
   end
 
-  def my_count
+  def my_count(arg = nil)
     count = 0
-    my_each { |value| count += 1 if yield value }
+    if block_given?
+      my_each { |value| count += 1 if yield value }
+    elsif arg
+      my_each { |value| count += 1 if value == arg }
+    else
+      my_each { count += 1 }
+    end
     count
   end
 
   def my_map(proc = nil)
+    return to_enum unless block_given?
+
     array = []
-    if proc.nil?
-      my_each do |value|
-        value = yield value
-        array << value
-      end
+    if proc
+      my_each { |value| array.push(proc.call(value)) }
     else
-      my_each do |value|
-        value = proc.call(value)
-        array << value
-      end
+      my_each { |value| array.push(yield(value)) }
     end
     array
   end
 
-  def my_inject(result = 0)
-    my_each { |value| result = yield(result, value) }
+  def my_inject(*arg)
+    arg[0].is_a?(Integer) ? initial = arg[0] : symbol = arg[0]
+    if initial && !arg[1].is_a?(Integer)
+      symbol = arg[1]
+    elsif arg.nil?
+      initial = self[0]
+    end
+    result = initial
+    if symbol
+      my_each { |value| result = result ? result.send(symbol, value) : value }
+    else
+      my_each { |value| result = result ? yield(result, value) : value }
+    end
     result
   end
 
-  def multiply_els(result = 1)
-    my_each { |value| result = yield(result, value) }
-    result
+  def multiply_els(arr)
+    arr.my_inject { |a, b| a * b }
   end
 end
 
-p %w[a b c d].my_each
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
